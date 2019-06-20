0817cb942e1625a201f1151669e8814491cf9ae4
Talent30 <jon.sun30@gmail.com>
2019-06-21 10:32:15 +1200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-nohup ss-server -p $SERVER_PORT -k $PASSWORD -t $TIMEOUT -m $METHOD -d $DNS_ADDRS -v -u --reuse-port >/var/log/shadowsocks.log 2>&1
-nohup ss-server -p $SERVER_PORT -k $PASSWORD -t $TIMEOUT -m $METHOD -d $DNS_ADDRS -v -u --reuse-port >/var/log/shadowsocks.log 2>&1
+ss-server -p $SERVER_PORT -k $PASSWORD -t $TIMEOUT -m $METHOD -d $DNS_ADDRS -v -u --reuse-port
+ss-server -p $SERVER_PORT -k $PASSWORD -t $TIMEOUT -m $METHOD -d $DNS_ADDRS -v -u --reuse-port
 exec “$@”
